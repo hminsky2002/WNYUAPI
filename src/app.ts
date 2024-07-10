13d6ca331afeda0bc7a6b84415daaffcc52d32cf
Harry Minsky <harry.minsky@gmail.com>
2024-07-10 10:57:42 -0400

    Add Pino Logger
    This commit adds the open-source pino library to the
    API, and runs a basic log on server listen. It also
    adds http logging as middleware for the app itself.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import { pinoHttp } from 'pino-http';
 import { rootRouter } from './routers';
+import { getLogger } from './logger';
+
+const logger = getLogger(__filename);
 
 const app = express();
 app.use(cors());
-
+app.use(
+  pinoHttp({
+    logger,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', rootRouter);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { app } from './app';
+import { getLogger } from './logger';
+
+const logger = getLogger(__filename);
 
 const port = Number(process.env.PORT ?? 3001);
 const host = process.env.HOST ?? 'localhost';
 
-app.listen(port, host, () => {});
+app.listen(port, host, () => {
+  logger.info(`Server running on http://${host}:${port}`);
+});
