85b65db1178a0caacca11a5fe78d5e205efcf158
Harry Minsky <harry.minsky@gmail.com>
2024-08-02 22:44:33 -0400

    Add Spins Caching
    This commit adds another cache to the API to store current spins.
    Notably, this cache utilizes another cache, the metadata cache,
    to read the current playlist id. This is the first time cache variables
    are being passed around, but future commits should take advantage of this
    functionality. Namely, writing in handlers that return custom combinations
    of the various caches to minimize the number of requests to our proxy.

--- src/handlers/spinsHandlers.ts
+++ src/handlers/spinsHandlers.ts
@@ -1,4 +1,42 @@
 import type { NextFunction, Request, Response } from 'express';
+import { currentPlaylist } from './playlistsHandlers';
+import { getLogger } from '../logger';
+import { SpinsResponse } from '@wnyu/spinitron-sdk';
+const logger = getLogger(__filename);
+
+let currentSpins: SpinsResponse | undefined;
+
+const UPCOMING_CACHE_DURATION = 3 * 60 * 1000;
+
+async function fetchCurrentSpins() {
+  try {
+    const url = `${process.env.SPINITRON_API_URL}/spins?${`playlist_id=${currentPlaylist?.items[0]?.id}&count=50` ?? ''}`;
+    const response = await fetch(url, {
+      headers: { Authorization: `Bearer ${process.env.SPINITRON_API_KEY}` },
+    });
+    const spinsData = (await response.json()) as SpinsResponse;
+    currentSpins = spinsData;
+    logger.info(`Spins updated at ${Date.now()}`);
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+setInterval(fetchCurrentSpins, UPCOMING_CACHE_DURATION);
+
+fetchCurrentSpins();
+
+const getCurrentSpins = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    res.send(currentSpins);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getSpins = async (
   req: Request,
@@ -42,4 +80,7 @@ const getSpinById = async (
 export const spinsHandlers = {
   getSpins,
   getSpinById,
+  getCurrentSpins,
 };
+
+export { currentSpins };
--- src/routers/spinsRouter.ts
+++ src/routers/spinsRouter.ts
@@ -5,5 +5,6 @@ const spinsRouter = express.Router();
 
 spinsRouter.get('/', spinsHandlers.getSpins);
 spinsRouter.get('/:id', spinsHandlers.getSpinById);
+spinsRouter.get('/current/spins', spinsHandlers.getCurrentSpins);
 
 export { spinsRouter };
